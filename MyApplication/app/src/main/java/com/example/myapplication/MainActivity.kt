4636040fe72f9631033cf08dfda87095a5ca1a55
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-06-20 16:37:40 +0530

    kotlin codelab 02

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Kshitij Bhardwaj")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+    private fun addNickname(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll() // resets the variable in resource
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MyName.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+import android.provider.ContactsContract
+
+data class MyName(
+    var name: String = "",
+    var nickname: String = ""
+)
